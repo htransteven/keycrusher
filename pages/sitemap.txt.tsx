a2a72d2ca9c97aef3003c5463230898cddebdbe5
Steven Huynh-Tran <htransteven@gmail.com>
2022-03-14 21:24:20 -0700

    add sitemap and robots.txt

---
+++ pages/sitemap.txt.tsx
@@ -0,0 +1,40 @@
+import { GetServerSideProps } from "next";
+
+const Sitemap = () => {
+  return null;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const BASE_URL = "http://localhost:3000";
+
+  const sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://keycrusher.com</loc>
+            <priority>1.0</priority>
+        </url>
+        <url>
+            <loc>https://keycrusher.com/daily</loc>
+            <priority>1.0</priority>
+        </url>
+        <url>
+            <loc>https://keycrusher.com/about</loc>
+            <priority>1.0</priority>
+        </url>
+        <url>
+            <loc>https://keycrusher.com/privacy</loc>
+            <priority>1.0</priority>
+        </url>
+    </urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
