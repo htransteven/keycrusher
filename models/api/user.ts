34162bb7af71c67b60993f18bcac793845738256
Steven Huynh-Tran <htransteven@gmail.com>
2022-03-22 16:19:05 -0700

    re add usernetwork in user models

--- models/api/user.ts
+++ models/api/user.ts
@@ -1,4 +1,8 @@
-import { UserNetwork } from "../firestore/Network";
+// differs from the UserNetwork interface in /models/api/firestore because the username is stored as key
+export interface UserNetwork {
+  followers: { [username: string]: boolean };
+  following: { [username: string]: boolean };
+}
 
 export interface User {
   username: string;
